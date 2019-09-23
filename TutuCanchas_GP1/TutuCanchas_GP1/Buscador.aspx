c32316304600fdd8acf7f275dcff1cb21cc05b17
Facuzer <45392697+Facuzer@users.noreply.github.com>
2019-09-23 00:56:30 -0300

    Quiero q me maten ya

--- TutuCanchas_GP1/TutuCanchas_GP1/Buscador.aspx
+++ TutuCanchas_GP1/TutuCanchas_GP1/Buscador.aspx
@@ -3,13 +3,11 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <body>
+    
         <br />
         <div class="container">
-
             <div class="row">
-            </div>
-                <div class="col-md-4" style="width: 100%; text-align: center;">
+                <div class="col-md-4" style="width: 100%; text-align: center; top: 0px; left: 300px;">
 
                     <h3 style="font-size: 20px; text-decoration: underline">Buscador</h3>
 
@@ -19,39 +17,26 @@
                     <br />
                     <asp:Label ID="Label2" runat="server" Text="Tipo de Cancha"></asp:Label>
                     <asp:DropDownList ID="ddTipoCancha" runat="server" class="custom-select d-block w-100" Width="100%"></asp:DropDownList>
-
-                    <br />
-                    <div class="row text-center">
-                        <asp:Label ID="Label3" runat="server" Text="Precio"></asp:Label>
-                    </div>
-                    <div class="col-md-4 text-center">
-                        <div id="precio" style="display:inline-block; ">
-                            <asp:TextBox ID="txPreciomin" runat="server" class="form-control" TextMode="Number" Width="50%" ></asp:TextBox>
-                        
-                            <asp:TextBox ID="txtPreciomax" runat="server" class="form-control" TextMode="Number" Width="50%"></asp:TextBox>
-                        </div>
-                    </div>
+                    <br />    
+                    <asp:Label ID="Label3" runat="server" Text="Precio" Width="100%"></asp:Label>
+                    <span><asp:TextBox ID="txPreciomin" runat="server"  class="form-control text-xl-center" TextMode="Number" Width="100%" ></asp:TextBox></span> 
+                    <span><asp:TextBox ID="txtPreciomax" runat="server" class="form-control text-xl-center" TextMode="Number" Width="100%"></asp:TextBox></span>
                     <br />
                     <asp:Label ID="Label4" runat="server" Text="Zonas"></asp:Label>
                     <asp:DropDownList ID="ddZonas" runat="server" class="custom-select d-block w-100" Width="100%" ></asp:DropDownList>
-
-                    <br 
-                    
-                    <div class="row">
-                        
-                            <asp:Label ID="Label5" runat="server">Fecha</asp:Label>
-                            <asp:Calendar ID="Calendar1" runat="server" ></asp:Calendar>
-                    
+                    <br />
+                    <div class="col-md-4" style=" width: 100%; text-align:center; left: 50px ">
+                            <asp:Label ID="Label5" runat="server"  Text="Fecha"></asp:Label>
+                            <br />
+                            <asp:Calendar ID="Calendar1" runat="server" Width="100%"></asp:Calendar>
                     </div>
                     <br />
                     <br />
-                    <asp:Button ID="btBuscar" runat="server" Text="Buscar" class="btn btn-primary btn-lg" OnClick="btBuscar_Click" Width="200px"  />
-
-                </div>
-                <div class="row">
-                </div>
-            
+                    <div class="row">
+                        <asp:Button ID="btBuscar" runat="server" Text="Buscar" class="btn btn-primary btn-lg " OnClick="btBuscar_Click" Width="100%" Font-Bold="true" />
+                    </div>
+                </div> 
+            </div>
         </div>
         
-    </body>
 </asp:Content>
--- TutuCanchas_GP1/TutuCanchas_GP1/Reservas.aspx.designer.cs
+++ TutuCanchas_GP1/TutuCanchas_GP1/Reservas.aspx.designer.cs
@@ -13,102 +13,12 @@ namespace TutuCanchas_GP1 {
     public partial class Reserva {
         
         /// <summary>
-        /// Control boxReserva1.
+        /// Control gvReservas.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox boxReserva1;
-        
-        /// <summary>
-        /// Control boxReserva2.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox boxReserva2;
-        
-        /// <summary>
-        /// Control boxReserva3.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox boxReserva3;
-        
-        /// <summary>
-        /// Control boxReserva4.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox boxReserva4;
-        
-        /// <summary>
-        /// Control boxReserva5.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox boxReserva5;
-        
-        /// <summary>
-        /// Control boxReserva6.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox boxReserva6;
-        
-        /// <summary>
-        /// Control boxReserva7.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox boxReserva7;
-        
-        /// <summary>
-        /// Control boxReserva8.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox boxReserva8;
-        
-        /// <summary>
-        /// Control boxReserva9.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox boxReserva9;
-        
-        /// <summary>
-        /// Control boxReserva10.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox boxReserva10;
-        
-        /// <summary>
-        /// Control btnVer.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnVer;
+        protected global::System.Web.UI.WebControls.GridView gvReservas;
     }
 }
