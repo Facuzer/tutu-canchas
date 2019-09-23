5e4203c42f88932939cbc2df46664facc6b59d15
Valentin Schiaffino <schiaffino.valentin@maimonidesvirtual.com.ar>
2019-09-22 22:07:05 -0300

    hola

---
+++ TutuCanchas_GP1/TutuCanchas.DAO/TipoCanchasDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TutuCanchas.DAO
+{
+    public static class TipoCanchasDAO
+    {
+        public static List<String> GetAllTipoCanchas()
+        {
+            Dictionary<Int32,String> TipoCanchas = new Dictionary<Int32, string>();
+
+
+        }
+    }
+}
