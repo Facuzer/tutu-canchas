e000d257d7fe8eeae1302db04ca8f758fbb47564
Valentin Schiaffino <schiaffino.valentin@maimonidesvirtual.com.ar>
2019-09-24 18:09:46 -0300

    toy triste

---
+++ TutuCanchas_GP1/TutuCanchas.Business/Reservas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TutuCanchas.Business
+{
+    public static class Reservas
+    {
+        public static List<DTO.ReservasDTO> GetAllReservas(int idUsuario)
+        {
+            return DAO.ReservasDAO.GetAllReservas(idUsuario);
+        }
+    }
+}
