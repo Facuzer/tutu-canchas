44a05f6652eedc3b98480a67b6f1edc89f93a11a
Facuzer <45392697+Facuzer@users.noreply.github.com>
2019-09-24 13:04:04 -0300

    una verga de perfil

--- TutuCanchas_GP1/TutuCanchas_GP1/Perfil.aspx
+++ TutuCanchas_GP1/TutuCanchas_GP1/Perfil.aspx
@@ -5,15 +5,39 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <div class="container">
         <div class="row"></div>
-        <div class="col-md-4">
-            <asp:Image ID="ftoperfil" runat="server" Height="152px"  ImageUrl="img/perfil.jpg" Width="152px"/>
-            <div class="col-md-12 text-center w-75" style="float: right">
-            <asp:Label ID="Label1" runat="server" Text="Nombre:"></asp:Label>
-            <asp:Label ID="Label2" runat="server" Text="None"></asp:Label>
+        <div id="box" class="col-md-12">
+            <asp:Image ID="ftoperfil" runat="server" Height="153px"  ImageUrl="img/perfil.jpg" Width="151px"/>
+            <div id="contenedor" class="col-md-12 w-auto">
+            <div id="text" class="row" style="display:inline-block">
+                <asp:Label ID="nomber" runat="server" Text="Nombre:" Width="58px"></asp:Label>
+                <asp:Label ID="Label2" runat="server" Text="None"></asp:Label>
+            </div>
+            <br />
+            <div class="row">
+                <asp:Label ID="Label3" runat="server" Text="Perfil:" Width="76px"></asp:Label>
+                <asp:Label ID="Label4" runat="server" Text="None"></asp:Label>
+            </div>
+            <div id="email" class="row">
+                <asp:Label ID="Label5" runat="server" Text="Email:" Width="76px"></asp:Label>
+                <asp:Label ID="Label6" runat="server" Text="None"></asp:Label>
+            </div>
+            <div id="telefono" class="row">
+                <asp:Label ID="Label7" runat="server" Text="Teléfono:" Width="76px"></asp:Label>
+                <asp:Label ID="Label8" runat="server" Text="None"></asp:Label>
+            </div>
             </div>
         </div>
     
     </div>
+    
+
+    
+    
+
+    
+    
+
+    
     
 
 </asp:Content>
--- TutuCanchas_GP1/TutuCanchas_GP1/Perfil.aspx.designer.cs
+++ TutuCanchas_GP1/TutuCanchas_GP1/Perfil.aspx.designer.cs
@@ -22,13 +22,13 @@ namespace TutuCanchas_GP1 {
         protected global::System.Web.UI.WebControls.Image ftoperfil;
         
         /// <summary>
-        /// Control Label1.
+        /// Control nomber.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
+        protected global::System.Web.UI.WebControls.Label nomber;
         
         /// <summary>
         /// Control Label2.
@@ -38,5 +38,59 @@ namespace TutuCanchas_GP1 {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// Control Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// Control Label4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+        
+        /// <summary>
+        /// Control Label5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+        
+        /// <summary>
+        /// Control Label6.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+        
+        /// <summary>
+        /// Control Label7.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label7;
+        
+        /// <summary>
+        /// Control Label8.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label8;
     }
 }
